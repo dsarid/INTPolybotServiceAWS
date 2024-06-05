43a94948b6ac098811f18e9a50a33eee35ccd5a3
Daniel S <daniel.sarid@tutanota.com>
2024-06-05 21:04:16 +0300

    added telegram toekn

--- polybot/app.py
+++ polybot/app.py
@@ -3,15 +3,48 @@ from flask import request
 import os
 from bot import ObjectDetectionBot
 import boto3
+from botocore.exceptions import ClientError
+import json
+
+
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+def get_secret():
+
+    secret_name = "telegram_bot_token"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return secret
+
+    # Your code goes here.
+
 
 app = flask.Flask(__name__)
 
 
-# TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+# TODO break down this line to multiple steps, for readability and error-checking
+TELEGRAM_TOKEN = json.loads(get_secret()).get('TELEGRAM_BOT_TOKEN')
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
-
 
 @app.route('/', methods=['GET'])
 def index():
